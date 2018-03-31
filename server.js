ae0a8b3234acbcc903e1db77d5a2ae3457942003
JrodManU <jrodmanu@yahoo.com>
2018-03-30 22:44:18 -0400

    added game list and related things

--- server.js
+++ server.js
@@ -14,17 +14,47 @@ const server = app.listen(port, () => {
   console.log("Server started on port 3000...")
 });
 
+let games = [];
+let sockets = [];
+let idCounter = 1;
+
+app.get("/get_games", (req, res) => {
+  res.json(games);
+});
+
 //Socket.io stuff
 io = socketIo(server, { transports: ['websocket', 'xhr-polling'] });
 
-io.on('connection', function(socket) {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('a user disconnected')
+io.on('connection', (socket) => {
+  sockets.push(socket);
+  console.log('A user connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('A user disconnected')
+    sockets.filter((item) => {
+      return item.id !== socket.id;
+    });
+  });
+
+  socket.on('create game', (name) => {
+    console.log("game " + name + " created");
+    games.push({
+      id: idCounter,
+      name: name,
+      player1Id: socket.id,
+      player2Id: null
+    });
+    idCounter++;
+    io.emit("update game list", JSON.stringify(games));
+  });
+
+  socket.on('join game', (id) => {
+    let game = games.find((item) => {
+      return item.id == id;
+    });
+    console.log("game " + game.name + " started");
+    game.player2Id = socket.id;
+    getSocket(game.player1Id).emit("game started", game);
+    socket.emit("game started", game);
   });
 });
-
-//update loop
-setInterval(function(){
-    io.emit('update time', Date.now());
-}, 100);
